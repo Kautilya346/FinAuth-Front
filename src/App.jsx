e5c7e93fdac323e075fda766640dbfc894f58f0d
Aditya Karanwal <adityakaranwal@gmail.com>
2025-10-09 16:20:58 +0530

    phone_VERIFICATION_fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/HomePage";
 import TransactionFormPage from "./pages/TransactionFormPage";
 import CsvUploadPage from "./pages/CsvUploadPage";
 import PhoneVerificationPage from "./pages/PhoneVerificationPage";
-import KycVerificationPage from "./pages/KycVerificationPage";
+import OnboardingRegistration from "./components/OnboardingRegistration";
 import Footer from "./components/Footer";
 import "./index.css";
 
@@ -13,12 +14,19 @@ function App() {
     <Router>
       <div className="min-h-screen bg-[#d4d9c8] flex flex-col">
         <Navbar />
+        <Toaster position="top-center" />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/transactions/new" element={<TransactionFormPage />} />
           <Route path="/upload-csv" element={<CsvUploadPage />} />
-          <Route path="/onboarding/phone-verification" element={<PhoneVerificationPage />} />
-          <Route path="/onboarding/kyc-verification" element={<KycVerificationPage />} />
+          <Route
+            path="/onboarding/phone-verification"
+            element={<PhoneVerificationPage />}
+          />
+          <Route
+            path="/kyc-registration"
+            element={<OnboardingRegistration />}
+          />
         </Routes>
         <Footer />
       </div>
